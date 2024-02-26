f77210fbec5d27437542fb71b5ffd937d7960314
kimdongju1 <dkdlkdh@naver.com>
2024-02-26 09:07:27 +0900

    3일차

--- day03/day03/Filedivi.cpp
+++ day03/day03/Filedivi.cpp
@@ -11,8 +11,11 @@ void AClass::AInfo() {
 }
 int main()
 {
-	AClass a(10);
-	a.AInfo();
+	
 
+	Human h("홍길동", 50, "백수");		// 객체생성방법
+	h.humanInfo();			// 나는 50살인 백수 홍길동입니다.
+	
+	
 	return 0;
 }
--- day03/day03/Filedivi.h
+++ day03/day03/Filedivi.h
@@ -7,7 +7,14 @@ class AClass {
 private:
 	int num;
 
-public:							// 접근제어지시자
-	AClass(int anum); 
-	void AInfo();
-	};
+public:				// 접근제어지시자
+	char name[20];
+	int age;
+	char job[20];
+
+	void showHuman()
+	{
+		printf("name: %s age: %d job: %s\n", name, age, job);
+	}
+	
+}Human;
